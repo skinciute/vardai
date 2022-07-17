6d1cf3734abaaca645bc56cdad32bdfc757ec706
skinciute <107060934+skinciute@users.noreply.github.com>
2022-07-17 20:39:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var vardai = ["Jon Snow", "Cersei Lannister","Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"]
+console.log(vardai[0])
+console.log(vardai[vardai.length -1])
+var skaičius = prompt("Įveskite skaičių nuo 1 iki 6")
+skaičius = Number(skaičius)
+console.log(vardai[skaičius])
